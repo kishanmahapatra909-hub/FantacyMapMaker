660e0491c5aba1c7db92cc1b2807145e9461dfd7
kishanmahapatra909-hub <kishanmahapatra909@gmail.com>
2026-05-25 00:33:18 +0530

    fix: update ball asset source path
    Correct the filename reference for the ball asset to ensure the image loads correctly.

--- src/constants.tsx
+++ src/constants.tsx
@@ -478,7 +478,7 @@ export const ASSETS = {
     { name: 'Antena', src: 'https://img.icons8.com/?size=100&id=SHYMAehqJN1p&format=png&color=000000', type: 'stage', subType: 'Antena' },
     { name: 'Test', src: images['pirate.png'], type: 'stage', subType: 'Antena' },
     
-    { name: 'Ball', src: images['ball.png'], type: 'stage', subType: 'Ball' },
+    { name: 'Ball', src: images['b1all.png'], type: 'stage', subType: 'Ball' },
     { name: 'Balloon', src: images['balloons.png'], type: 'stage', subType: 'Balloon' },
     { name: 'Bar', src: images['bar.png'], type: 'stage', subType: 'Bar' },
     { name: 'Chilling', src: images['chilling.png'], type: 'stage', subType: 'Chilling' },
