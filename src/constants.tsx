882bd4c8fea80ba72c123e278335deb8954b5719
kishanmahapatra909-hub <kishanmahapatra909@gmail.com>
2026-05-24 22:25:45 +0530

    refactor: comment out unused assets
    Temporarily remove unused assets from constants to 
    clean up the UI component library.

--- src/constants.tsx
+++ src/constants.tsx
@@ -476,14 +476,14 @@ export const ASSETS = {
     { name: 'Radar 2', src: 'https://img.icons8.com/?size=100&id=NkX2DLO7ATck&format=png&color=000000', type: 'stage', subType: 'Radar 2' },
     { name: 'Antena', src: 'https://img.icons8.com/?size=100&id=SHYMAehqJN1p&format=png&color=000000', type: 'stage', subType: 'Antena' },
     { name: 'Test', src: images['pirate.png'], type: 'stage', subType: 'Antena' },
-    
+    /*
     { name: 'Ball', src: images['ball.png'], type: 'stage', subType: 'Ball' },
     { name: 'Balloon', src: images['balloons.png'], type: 'stage', subType: 'Balloon' },
     { name: 'Bar', src: images['bar.png'], type: 'stage', subType: 'Bar' },
     { name: 'Chilling', src: images['chill.png'], type: 'stage', subType: 'Chilling' },
     { name: 'Circus Bear', src: images['bear.png'], type: 'stage', subType: 'Circus Bear' },
 
-    
+    */
   
   
   ],
