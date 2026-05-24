01e0cbf678c22e6380914993c9e51c93b1f67a9c
kishanmahapatra909-hub <kishanmahapatra909@gmail.com>
2026-05-24 20:49:31 +0530

    feat: add new artifact assets to image registry
    Update Artifacts.js to include additional game assets 
    (ball, balloons, bar, chilling, bear) and integrate 
    them into the project constants for use in the board 
    creator.

---
+++ src/assets/images/Artifacts.js
@@ -0,0 +1,30 @@
+/*import pirateImg from './pirate_island_quest_map_1779210700182.png';
+import aan from './aan.jpg';
+import bbn from './bbn.jpg';
+
+// This exports the folder contents as a single object map
+export const images = {
+  'pirate.png': pirateImg,
+  'aan.jpg': aan,
+  'bbn.jpg': bbn
+};*/
+// ./assets/images/Artifacts.js
+
+import pirateImg from './pirate_island_quest_map_1779210700182.png';
+import Ball from './Ball.png';
+import Balloons from './balloons.png';
+import bar from './bar.png';
+import chill from './chilling.png';  
+import bear from './Circus bear.png';
+
+
+
+// This exports the folder contents as a single object map
+export const images = {
+  'pirate.png': pirateImg,
+  'ball.png': Ball,
+  'balloons.png': Balloons,
+  'bar.png': bar,
+  'chill.png': chill,
+  'bear.png': bear,
+};
